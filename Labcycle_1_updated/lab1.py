d01b9a634805cf341d95c998cd3136ba33e5956d
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2022-04-18 22:50:49 +0530

    Update lab1.py

--- Labcycle_1_updated/lab1.py
+++ Labcycle_1_updated/lab1.py
@@ -9,11 +9,19 @@ def digits(x):
    dig_2=(x//10)%10
    dig_3=(x//100)%10
    dig_4=(x//1000)%10
+   return dig_1,dig_2,dig_3,dig_4
+def sum_dig(dig_1,dig_2,dig_3,dig_4):
    sum=dig_1+dig_2+dig_3+dig_4
    print("Sum of the digits of ",x,"is : ",sum)
-   print("Reverse of ",x,"is :",dig_1,dig_2,dig_3,dig_4)
+def reverse(dig_1,dig_2,dig_3,dig_4):
+  reverse = (dig_1*1000) + (dig_2*100) + (dig_3*10) + dig_4
+  print("The reverse of ",x,"is : ",reverse)
+def prdt_dig(dig_1,dig_2,dig_3,dig_4):
    evenpr=dig_1*dig_3
    oddpr=dig_2*dig_4
-   print("Difference between the product of digits at the odd position and the product of digits at the even position: ",oddpr-evenpr)
+   print("Difference of the product: ",oddpr-evenpr)
 x=int(input("Enter a 4 digit number : "))
-digits(x)
+d1,d2,d3,d4=digits(x)  
+sum_dig(d1,d2,d3,d4)
+reverse(d1,d2,d3,d4)
+prdt_dig(d1,d2,d3,d4)
